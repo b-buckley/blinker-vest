f88f0d6deb253f49618ac9af439e37c8f50b25f5
Ben Buckley <ben.buckley.vt@gmail.com>
2022-03-15 18:17:32 -0400

    Added dirty separate handling for each input signal as proof-of-concept.

--- ws2811_test/ws2811_test.ino
+++ ws2811_test/ws2811_test.ino
@@ -6,33 +6,100 @@
 #include "FastLED.h"
 
 #define PIN_LED 13
-#define PIN_SIGNAL 5
+#define PIN_LEFT_SIG 5
+#define PIN_BRAKE_SIG 6
+#define PIN_RIGHT_SIG 7
 #define PIN_DATA 12
 
-#define NUM_PIXELS 1
+#define NUM_PIXELS 3
+#define LEFT_OUT 2
+#define BRAKE_OUT 1
+#define RIGHT_OUT 0
 
-int signal;
+#define DELAY 1000
+
+
+int sigLeft,
+    sigBrake,
+    sigRight,
+    signal = 0,
+    previousTime = 0,
+    currentTime,
+    sigLeftMark = 0,
+    sigLeftCurrTime,
+    sigLeftSeconds,
+    sigRightMark = 0,
+    sigRightCurrTime,
+    sigRightSeconds,
+    i;
 
 CRGB leds[NUM_PIXELS];
 
 void setup() {
     signal = 0;
     pinMode(PIN_LED,OUTPUT);
-    pinMode(PIN_SIGNAL,INPUT);
+    pinMode(PIN_LEFT_SIG,INPUT);
+    pinMode(PIN_BRAKE_SIG,INPUT);
+    pinMode(PIN_RIGHT_SIG,INPUT);
     digitalWrite(PIN_LED,LOW);
 
     FastLED.addLeds<WS2811, PIN_DATA>(leds, NUM_PIXELS);
 }
 
 void loop() {
-    signal = digitalRead(PIN_SIGNAL);
 
-    if (signal) {
-      leds[0] = CRGB::Blue;
+    previousTime = currentTime;
+    currentTime = millis();
+
+    //One var for each signal... replace with ext. interrupts?
+    sigLeft = digitalRead(PIN_LEFT_SIG);
+    sigBrake = digitalRead(PIN_BRAKE_SIG);
+    sigRight = digitalRead(PIN_RIGHT_SIG);
+
+    //Quick-n-dirty brake signal handling
+    if (sigBrake) {
+        leds[BRAKE_OUT] = CRGB(255,0,0);
     }
     else {
-      leds[0] = CRGB::Red;
+        leds[BRAKE_OUT] = CRGB(100,0,0);
     }
+
+    //Quick and dirty handle of the left turn signal
+    if (sigLeft) {
+        
+        if(sigLeftMark == 0) sigLeftMark = millis();
+        sigLeftCurrTime = millis();
+        sigLeftSeconds = (sigLeftCurrTime - sigLeftMark) / 1000;
+        if (sigLeftSeconds % 2) {
+            leds[LEFT_OUT] = CRGB::Yellow;
+        }
+        else {
+            leds[LEFT_OUT] = CRGB::Black;
+        }
+    }
+    else {
+        leds[LEFT_OUT] = CRGB::Black;
+    }
+
+    //Right turn handling
+    if (sigRight) {
+        
+        if(sigRightMark == 0) sigRightMark = millis();
+        sigRightCurrTime = millis();
+        sigRightSeconds = (sigRightCurrTime - sigRightMark) / 1000;
+        if (sigRightSeconds % 2) {
+            leds[RIGHT_OUT] = CRGB::Yellow;
+        }
+        else {
+            leds[RIGHT_OUT] = CRGB::Black;
+        }
+    }
+    else {
+        leds[RIGHT_OUT] = CRGB::Black;
+    }
+    
+
+    //Send the resulting frame to LED data out
     digitalWrite(PIN_LED,signal);
     FastLED.show();
 }
